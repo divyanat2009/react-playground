7b22a39cef5fd4a2f081c2f41e27e440a1570ed5
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-11-14 00:57:03 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import HelloWorld from './state-drills/HelloWorld';
+import Bomb from './state-drills/Bomb';
+import RouletteGun from './state-drills/RouletteGun';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render() { 
+    return (  
+      <div className="App">
+        <h2>Hello World!</h2>
+        <HelloWorld></HelloWorld>
+        <h2>Bomb</h2>
+        <Bomb></Bomb>
+        <h2>RouletteGun</h2>
+        <RouletteGun bulletInChamber={8}></RouletteGun>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
